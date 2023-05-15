4a49949b9899f03278158b8e686671d6c34c40e1
gdarandale <gdarandale@LT-AS-IND-39.SSGCORP.local>
2023-05-15 06:54:26 +0530

    test

---
+++ azureEg-2/src/main/java/com/example/AzureEg/AzureEg2Application.java
@@ -0,0 +1,13 @@
+package com.example.AzureEg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AzureEg2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AzureEg2Application.class, args);
+	}
+
+}
---
+++ azureEg-2/src/test/java/com/example/AzureEg/AzureEg2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.AzureEg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AzureEg2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
